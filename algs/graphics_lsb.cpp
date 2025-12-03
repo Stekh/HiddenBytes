d61bd1795e46fa7a0c9c076683c1fc4ab6e66bf9
Stekh <bartoszstec@onet.pl>
2025-12-03 19:22:49 +0100

    implement graphics_lsb decoding

--- algs/graphics_lsb.cpp
+++ algs/graphics_lsb.cpp
@@ -13,7 +13,7 @@ namespace gr {
 		QImage image = load_image(path);
 
 		QByteArray msg = message.toUtf8();
-		size_t msg_len = msg.size();
+		const size_t msg_len = msg.size();
 		size_t msg_iter = 0;
 		size_t bit_iter = 0;
 
@@ -22,16 +22,16 @@ namespace gr {
 		}
 
 		uchar *data = image.bits();
-		size_t width = image.width();
-		size_t height = image.height();
-		size_t bytes_per_line = image.bytesPerLine();
+		const size_t width = image.width();
+		const size_t height = image.height();
+		const size_t bytes_per_line = image.bytesPerLine();
 
 		bool stop = false;
 
 		for (size_t y = 0; y < height; y++) {
 			for (size_t x = 0; x < width; x++) {
 				// there are 4 bytes per pixel, accessible as data[offset+a] where a is a value between 0 and 3
-				size_t offset = y * bytes_per_line + x * 4;
+				const size_t offset = y * bytes_per_line + x * 4;
 				if (bit_iter == 3) {
 					bit_iter %= 2;
 					msg_iter++;
@@ -41,8 +41,8 @@ namespace gr {
 					}
 				}
 				for (size_t b = 0; b < 4; b++) {
-					if ((data[offset+b] & (1 << 1)) ^ (msg[msg_iter] & (1 << (b + 4*bit_iter)))) {
-						data[offset] ^= 1 << 1;
+					if ((data[offset + b] & 1) ^ (msg[msg_iter] & (1 << (b + 4 * bit_iter)))) {
+						data[offset + b] ^= 1 << 1;
 					}
 					bit_iter++;
 				}
@@ -55,6 +55,58 @@ namespace gr {
 		return QImage(data, width, height, QImage::Format_RGB32);
 	}
 
-	QString lsb_decode(QString path) { return ""; }
+	QString lsb_decode(QString path) {
+		QImage image = load_image(path);
+
+		if (image.format() != QImage::Format_RGB32) {
+			image = image.convertToFormat(QImage::Format_RGB32);
+		}
+
+		const uchar *data = image.bits();
+		const size_t width = image.width();
+		const size_t height = image.height();
+		const size_t bytes_per_line = image.bytesPerLine();
+
+		QByteArray result;
+		size_t upper_half = 0;
+		size_t msg_cnt = 0;
+		result.append('\0');
+
+		bool stop = false;
+
+		for (size_t y = 0; y < height; y++) {
+			for (size_t x = 0; x < width; x++) {
+				// there are 4 bytes per pixel, accessible as data[offset+a] where a is a value between 0 and 3
+				const size_t offset = y * bytes_per_line + x * 4;
+				quint8 current_byte = static_cast<quint8>(result.at(msg_cnt));
+				for (size_t b = 0; b < 4; b++) {
+					if ((data[offset + b] & 1) == 1) {
+						current_byte |= 1 << (b + 4 * upper_half);
+					} else {
+						current_byte &= ~(1 << (b + 4 * upper_half));
+					}
+				}
+
+				result[msg_cnt] = static_cast<char>(current_byte);
+				++upper_half %= 2;
+
+				if (x > 0 && x % 2 == 0) {
+					msg_cnt++;
+					result.append('\0');
+				}
+
+				if (upper_half == 1 && current_byte == 0) {
+					stop = true;
+					break;
+				}
+			}
+
+			if (stop) {
+				break;
+			}
+		}
+
+		return QString(result);
+	}
 
 } // namespace gr
